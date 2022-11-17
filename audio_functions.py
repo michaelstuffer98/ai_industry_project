2034403e2101c53ebd62d82a9ef10d1603ba29bf
michaelSt9898 <michi_stuffer@outlook.de>
2022-11-17 16:13:22 +0100

    Add some descriptions and comments

--- audio_functions.py
+++ audio_functions.py
@@ -1,4 +1,3 @@
-from pydub import AudioSegment
 import librosa as lr
 import soundfile as sf
 import numpy as np
@@ -8,21 +7,57 @@ Introduces some methods to perform preprocessing on audio data
 """
 
 def slice_to_length(audiofile, sr, length_ms, pad=True):
+    """
+    slices the audiofile to the desired length from the beginning, discards the rest of the audio
+        Parameters:
+            audiofile: n-dimensional numpy array
+            sr: sample rate of the provided audio
+            length_ms: length of the sliced audio array
+            pad: if True, audio is zero padded at the end if the provided audio is shorter than the length_ms
+        returns: first length_ms milliseconds of the passed audiofile
+    """
     length_index = int(sr*length_ms/1000)
     if pad and audiofile.shape[0] < length_index:
         return np.concatenate((audiofile, np.zeros((length_index - audiofile.shape[0], 2), dtype=np.float32)), axis=0)
     return audiofile[0:length_index, :]
 
 def to_mono_channel(audiofile):
+    """
+    converts a audiofile with multiple channels to a single (mono) channel audio file
+        Parameters:
+            audiofile: n-dimensional numpy array
+        returns: 1-dimensional numpy array
+    """
     return lr.to_mono(audiofile.T).T
 
 def resample(audiofile, old_sample_rate, new_sample_rate):
+    """
+    resamples the audiofile from old_sample_rate to the new_sample_rate
+        Parameters:
+            audiofile: n-dimensional numpy array
+            old_sample_rate: sample rate of the provided audio file
+            new_sample_rate: desired sampling rate
+        returns: 1-dimensional numpy array with the new sample rate
+    """
     return lr.resample(audiofile.T, orig_sr=old_sample_rate, target_sr=new_sample_rate).T
 
-def channels(audiofile):
+def channels(audiofile) -> int:
+    """
+    Get the number of channels of the audio file
+        Parameters:
+            audiofile: n-dimensional numpy array
+        returns: nr of channels of the audiofile
+    """
     return 1 if len(audiofile.shape) <= 1 else audiofile.shape[1]
 
-def duration(audiofile, sample_rate):
+def duration(audiofile, sample_rate) -> float:
+    """
+    gets the duration of the audio file in seconds
+        Parameters:
+            audiofile: n-dimensional numpy array
+            sr: sampling_rate
+        returns: duration of the audiofile in seconds
+    """
     return audiofile.shape[0] / sample_rate
 
 def test():
--- plot.py
+++ plot.py
@@ -2,5 +2,10 @@ import matplotlib.pyplot as plt
 import librosa.display as lrd
 
 def plot_waveform(amp, sr):
+    """
+    Plot a waveform of the given amplitudes
+        amp: amplitudes as a numpy array
+        sr: sample rate of the audio
+    """
     plt.figure(figsize=(14, 5))
     lrd.waveshow(amp, sr=sr)
