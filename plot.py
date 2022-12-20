392640f0255246e18d144bbb431678fba8a53cc6
mstuffer <mstuffer@soundhound.com>
2022-12-20 17:04:39 +0100

    add training history plotting

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import librosa.display as lrd
 import numpy as np
 import librosa
+
+
 def plot_waveform(amp, sr):
     """
     Plot a waveform of the given amplitudes
@@ -21,3 +23,17 @@ def plot_melspectogram(S, sr, fmax, save_fig=True):
     ax.set(title='Mel-frequency spectrogram')
     if save_fig:
         fig.savefig('Plots/melfrequency_spectogram with fmax{f}.jpg'.format(f=int(fmax)))
+
+def plot_hist(history, keys, legends, title, y_label, x_label):
+    max_size = 0
+    for key in keys:
+        if max_size < len(history[key]):
+            max_size = len(history[key])
+        plt.plot(history[key])
+    # plt.xticks(range(max_size))
+    plt.title(title)
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.grid()
+    plt.legend(legends, loc='upper left')
+    plt.show()
